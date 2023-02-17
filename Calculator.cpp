189bb8659e957f1ec19b1b193798f19e58e31a4c
Pradhyumna-Joshi <92945755+Pradhyumna-Joshi@users.noreply.github.com>
2023-02-17 19:38:19 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+int add(int a,int b){
+  return a+b;  
+} 
+
+int sub(int a,int b){
+  return a-b;
+}
+
+int mul(int a,int b){
+  return a*b;
+}
+
+int div(int a,int b){
+  return a/b;
+}
+
+
+int main(){
+  
+  cout<<add(5,3)<<endl;
+  cout<<sub(5,3)<<endl;
+  cout<<mul(5,3)<<endl;
+  cout<<div(4,2)<<endl;
+  
+  return 0;
+}  
+  
