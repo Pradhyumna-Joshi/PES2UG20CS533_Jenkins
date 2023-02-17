e24f7ba25bd59f41c904ac1fbbb426236b5eebab
Pradhyumna-Joshi <92945755+Pradhyumna-Joshi@users.noreply.github.com>
2023-02-17 19:51:21 +0530

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -14,7 +14,7 @@ int mul(int a,int b){
   return a*b;
 }
 
-int div(int a,int b){
+int div_(int a,int b){
   return a/b;
 }
 
@@ -24,7 +24,7 @@ int main(){
   cout<<add(5,3)<<endl;
   cout<<sub(5,3)<<endl;
   cout<<mul(5,3)<<endl;
-  cout<<div(4,2)<<endl;
+  cout<<div_(4,2)<<endl;
   
   return 0;
 }  
